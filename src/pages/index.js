2a7569a91b9499be48873407baf72bafa3705b19
yumimukai <tourokuma4@gmail.com>
2020-09-05 22:08:29 +0900

    deploy commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
@@ -16,8 +15,7 @@ const IndexPage = () => (
       <Image />
     </div>
     <GitChart/>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+
   </Layout>
 )
 
